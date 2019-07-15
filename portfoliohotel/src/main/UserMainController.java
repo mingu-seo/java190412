de8650b3e37170c6701a980c7a002da10a5b0ab3
현수 <hyeonsu2014@naver.com>
2019-07-15 18:18:52 +0900

    room 패키지 추가

--- portfoliohotel/src/main/UserMainController.java
+++ portfoliohotel/src/main/UserMainController.java
@@ -21,27 +21,5 @@ public class UserMainController {
 		return "pkg/special_promotion";
 	}
 	
-	@RequestMapping("/room/room-detail-subpage.do")
-	public String room_detail_subpage(Model model, AdminVO param) throws Exception {
-		
-		return "room/room-detail-subpage";
-	}
 	
-	@RequestMapping("/room/room-detail-subpage2.do")
-	public String room_detail_subpage2(Model model, AdminVO param) throws Exception {
-		
-		return "room/room-detail-subpage2";
-	}
-	
-	@RequestMapping("/room/room-detail-subpage3.do")
-	public String room_detail_subpage3(Model model, AdminVO param) throws Exception {
-		
-		return "room/room-detail-subpage3";
-	}
-	
-	@RequestMapping("/room/room-detail-subpage4.do")
-	public String room_detail_subpage4(Model model, AdminVO param) throws Exception {
-		
-		return "room/room-detail-subpage4";
-	}
 }
---
+++ portfoliohotel/src/room/RoomController.java
@@ -0,0 +1,35 @@
+package room;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import manage.admin.AdminVO;
+
+@Controller
+public class RoomController {
+	
+	@RequestMapping("/room/room-detail-subpage.do")
+	public String room_detail_subpage(Model model, AdminVO param) throws Exception {
+		
+		return "room/room-detail-subpage";
+	}
+	
+	@RequestMapping("/room/room-detail-subpage2.do")
+	public String room_detail_subpage2(Model model, AdminVO param) throws Exception {
+		
+		return "room/room-detail-subpage2";
+	}
+	
+	@RequestMapping("/room/room-detail-subpage3.do")
+	public String room_detail_subpage3(Model model, AdminVO param) throws Exception {
+		
+		return "room/room-detail-subpage3";
+	}
+	
+	@RequestMapping("/room/room-detail-subpage4.do")
+	public String room_detail_subpage4(Model model, AdminVO param) throws Exception {
+		
+		return "room/room-detail-subpage4";
+	}
+}
