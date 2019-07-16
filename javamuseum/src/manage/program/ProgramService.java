72868de2d097ad166f6c8ee816de8c1040280b51
멋쟁이쩐 <1004584@naver.com>
2019-07-16 16:18:36 +0900

    program 수정

---
+++ javamuseum/src/manage/program/ProgramService.java
@@ -0,0 +1,98 @@
+package manage.program;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgramService {
+
+	@Autowired
+	private ProgramDAO programDao;
+	
+//	public int[] count(ProgramVO vo) throws Exception {
+//		int rowCount = programDao.count(vo);
+//		int[] rowPageCount = new int[2];
+//		int pageCount = Page.getPageCount(vo.getPageRows(), rowCount);
+//		rowPageCount[0] = rowCount;
+//		rowPageCount[1] = pageCount;
+//		return rowPageCount;
+//	}
+
+	public ArrayList list(ProgramVO vo) throws Exception {
+		ArrayList list = programDao.list(vo);
+		return list;
+	}
+
+//	public int insert(ProgramVO vo, HttpServletRequest request) throws Exception {
+//		
+//		FileUtil fu = new FileUtil();
+//		Map fileMap = fu.getFileMap(request);
+//		MultipartFile file= (MultipartFile)fileMap.get("filename_tmp");
+//		if (!file.isEmpty()) {
+//			fu.upload(file, SiteProperty.NOTICE_UPLOAD_PATH, SiteProperty.REAL_PATH, "notice");
+//			vo.setFilename(fu.getName());
+//			vo.setFilename_org(fu.getSrcName());
+//			vo.setFilesize(fu.getSrcSize());
+//		}
+//		
+//		int lastNo = (Integer)programDao.insert(vo);
+//		
+//		return lastNo;
+//	}
+//	
+//	public ProgramVO read(ProgramVO vo, boolean userCon) throws Exception {
+//		ProgramVO data = programDao.read(vo);
+//		if (userCon) {	// 사용자쪽 조회시만 update
+//			programDao.updateReadno(vo);
+//		}
+//		return data;
+//	}
+//
+//	public int update(ProgramVO vo) throws Exception {
+//		ProgramVO data = programDao.read(vo);
+//		int r = (Integer)programDao.update(vo);
+//		if(r > 0){
+//			if("1".equals(vo.getFilename_chk()) || !"".equals(Function.checkNull(vo.getFilename()))){
+//				Function.fileDelete(vo.getUploadPath(), data.getFilename());
+//			}
+//		}
+//		return r;
+//	}
+//
+//	
+//	public int delete(ProgramVO vo) throws Exception {
+//		ProgramVO data = programDao.read(vo);
+//		int r = programDao.delete(vo);
+//		if (r > 0) {
+//			Function.fileDelete(vo.getUploadPath(), data.getFilename());
+//		}
+//		return r;
+//	}
+//	
+//	public int groupDelete(ProgramVO vo, HttpServletRequest request) throws Exception {
+//		String[] nos = request.getParameterValues("no");
+//		int delCount = 0;
+//		if (nos.length > 0) {
+//			for (int i=0; i<nos.length; i++) {
+//				ProgramVO nvo = new ProgramVO();
+//				nvo.setNo(Function.getIntParameter(nos[i]));
+//				ProgramVO data = programDao.read(vo);
+//				int r = programDao.delete(vo);
+//				if (r > 0) {
+//					delCount++;
+//					Function.fileDelete(vo.getUploadPath(), data.getFilename());
+//				}
+//			}
+//		}
+//		return delCount;
+//	}
+
+	/*
+	public ArrayList mainList(ProgramVO vo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+*/
+}
